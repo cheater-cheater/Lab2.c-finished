1a63a2d08b8fa28aa60f5d010327d014d9098cf5
cheater-cheater <cheater@clemson.edu>
2025-01-31 13:20:43 -0500

    Add files via upload

---
+++ fixed.c
@@ -0,0 +1,21 @@
+/* errors.c
+	Charles M Heater
+	CpSc 1011, Section 002 , Spring 2025
+	Due 1/24/25
+	Part 2 of Lab #2
+	Fixing the errors in the file.
+*/
+
+#include <stdio.h>
+
+int main(void){
+	int sum;
+
+	/* compute result*/
+	sum = 5 + 25 * 2;
+
+	/* display results */
+	printf("The answer is %d\n", sum);
+
+	return 0;
+}
---
+++ lab2.c
@@ -0,0 +1,36 @@
+/* Charles M Heater
+CpSc 1011, Lab Section 002, Spring 2025
+Due 1/24/25
+Part 1 of Lab#2
+My first C program:
+print "I love programming!" to the screen.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+
+	return 0;
+}
+
+
